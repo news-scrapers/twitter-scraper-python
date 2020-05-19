2329bff055433a12c949fbe9fd94c15618f4a370
hugo <hjbello.wk@gmail.com>
2020-05-19 19:48:32 +0200

    fix naming

--- models/Config.py
+++ models/Config.py
@@ -10,7 +10,7 @@ class Config:
         db = client['tweets']
         self.scraping_id = None
         self.accounts = None
-        self.tag = None
+        self.account_list_file = None
         self.account_index = 0
         self.collection=db.Config
         self.load_from_json()
@@ -21,8 +21,8 @@ class Config:
         with open('./scraping_config.json', 'r') as f:
             result = json.load(f)
             self.load_from_dict(result)
-            if not self.tag == None:
-                with open('./account_lists/'+ self.tag + '.json', 'r') as f:
+            if not self.account_list_file == None:
+                with open('./account_lists/'+ self.account_list_file , 'r') as f:
                     accounts = json.load(f)
                     print(accounts)
                     self.accounts = accounts
@@ -37,11 +37,11 @@ class Config:
     def load_from_dict(self, result):
         self.accounts = result["accounts"]
         self.scraping_id = result["scraping_id"]
-        self.tag = result["tag"]
+        self.account_list_file = result["account_list_file"]
 
     def update(self):
         self.collection.find_one_and_update( {"scraping_id" : self.scraping_id},{"$set": self.serialize()},upsert=True)
 
     def serialize(self):
-        obj = {"accounts": self.accounts, "account_index":self.account_index, "last_updated":datetime.datetime.now(),"scraping_id":self.scraping_id,  "tag":self.tag}
+        obj = {"accounts": self.accounts, "account_index":self.account_index, "last_updated":datetime.datetime.now(),"scraping_id":self.scraping_id,  "account_list_file":self.account_list_file}
         return obj
